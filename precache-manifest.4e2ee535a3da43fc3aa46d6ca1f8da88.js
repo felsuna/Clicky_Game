389b3ec7dc74e9d34f063b7fe2941154c01d2849
felsuna <felicia.sunanto@gmail.com>
2019-03-20 21:44:06 -0700

    Updates

---
+++ precache-manifest.4e2ee535a3da43fc3aa46d6ca1f8da88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  },
+  {
+    "revision": "adb656e99515a3cfdf8e",
+    "url": "/Clicky_Game/static/js/main.be10dc07.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/Clicky_Game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "adb656e99515a3cfdf8e",
+    "url": "/Clicky_Game/static/css/main.a63b273e.chunk.css"
+  },
+  {
+    "revision": "ca21e30dccfedc467e2610b7c0514d26",
+    "url": "/Clicky_Game/index.html"
+  }
+];
